fefe0a2f3bb77dc8b5aa9224fcbb9444cc3ef707
Were Lenox <104990838+lenodewere@users.noreply.github.com>
2024-08-02 13:56:42 +0300

    Lennie
    Lennie

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Medical Practitioner . . ." , "Consultant . . ." , "Doctor . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
